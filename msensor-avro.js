62ba524d72c5558b162c2499268a7814b2137fb6
Vladimir POPOV <vladimir.popov@free.fr>
2023-11-02 21:10:39 +0100

    multi-line edit works

--- msensor-avro.js
+++ msensor-avro.js
@@ -1,35 +1,15 @@
 module.exports = function(RED) {
-    function MsensorNode(config) {
-        RED.nodes.createNode(this.config);
+
+    function MsensorAvro(config) {
+        RED.nodes.createNode(this,config);
         var node = this;
-        node.on('input', function(msg, send, done) 
-        {
-            error(msg)
-            // do something with 'msg'
-            msg.payload = msg.payload;
-            send(msg);
-
-            // Once finished, call 'done'.
-            // This call is wrapped in a check that 'done' exists
-            // so the node will work in earlier versions of Node-RED (<1.0)
-//            err = ""
-            if (done) {
-//                done(err);
-                done();
-            }
+        node.avroSchema = config.avroSchema || "default avro schema"
+        node.on('input', function(msg) {
+            node.log(node.avroSchema);
+            msg.payload = msg.payload.toLowerCase() + node.avroSchema.toString();
+            node.send(msg);
         });
-        
-        node.on('close', function(removed, done) {
-            // tidy up any state
-            if (removed){
-                // this node has been disabled/deleted
-            }
-            else {
-                // this node is being restarted
-            }
-            done();
-        });
-
     }
-    RED.nodes.registerType("msensor-avro", MsensorNode);
+    RED.nodes.registerType("msensor-avro",MsensorAvro);
 }
+
