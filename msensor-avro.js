0c3b25fd36ff04a80d970b473273200b06ce5b4f
Vladimir POPOV <vladimir.popov@free.fr>
2023-11-02 22:45:19 +0100

    getting avro hooked up. Not working yet

--- msensor-avro.js
+++ msensor-avro.js
@@ -1,14 +1,29 @@
-module.exports = function(RED) {
+var avro = require('avro-js')
 
+module.exports = function(RED) {
     function MsensorAvro(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.avroSchema = config.avroSchema || "default avro schema"
-        node.on('input', function(msg) {
-            node.log(node.avroSchema);
-            msg.payload = msg.payload.toLowerCase() + node.avroSchema.toString();
-            node.send(msg);
-        });
+        var msensor_type = avro.parse(node.avroSchema);
+
+        node.on('input', function(msg, send, done) {
+            try {
+                if (Buffer.isBuffer(msg.payload.value)) {
+                    msg.payload = msensor_type.fromBuffer(msg.payload.value);
+                    node.error(msg.payload['temperature']);
+                    send(msg);
+                } else {
+                    node.error("Payload is not Avro: " + msg.payload.value);
+                }
+                if (done) {
+                    done();
+                }
+            }
+            catch(err) {
+                node.error(err.message);
+            }                    
+    });
     }
     RED.nodes.registerType("msensor-avro",MsensorAvro);
 }
